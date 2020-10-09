e6b041be8d30dc7ffe8a89905720cd043bbdd037
Marcelo Santos <margersan@gmail.com>
2020-10-09 19:53:07 -0300

    Product entity, SQL script

---
+++ backend/src/main/java/com/exactorysolutions/dscatalog/entities/Product.java
@@ -0,0 +1,108 @@
+package com.exactorysolutions.dscatalog.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name; 
+	
+	//Anotation para permitir textos longos
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	private Double price;
+	private String img_url;
+	
+	//Anotation para gravar as informações no banco como UTC
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant date;
+	
+	@ManyToMany //Relacionamento muitos para muitos
+	@JoinTable(name = "tb_product_category", //nome da tabela NtoN e nome das chaves
+		joinColumns = @JoinColumn(name = "product_id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id"))
+	Set<Category> categories = new HashSet<>();
+	
+	public Product() {
+	}
+
+	public Product(Long id, String name, String description, Double price, String img_url, Instant date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.img_url = img_url;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImg_url() {
+		return img_url;
+	}
+
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+}
