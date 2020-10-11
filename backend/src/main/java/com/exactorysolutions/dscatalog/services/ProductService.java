818becb60d9fc65e2bb2897a7365a25d1496503f
Marcelo Santos <margersan@gmail.com>
2020-10-11 20:49:45 -0300

    Product insert, update, delete

--- backend/src/main/java/com/exactorysolutions/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/exactorysolutions/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.exactorysolutions.dscatalog.dto.CategoryDTO;
 import com.exactorysolutions.dscatalog.dto.ProductDTO;
+import com.exactorysolutions.dscatalog.entities.Category;
 import com.exactorysolutions.dscatalog.entities.Product;
+import com.exactorysolutions.dscatalog.repositories.CategoryRepository;
 import com.exactorysolutions.dscatalog.repositories.ProductRepository;
 import com.exactorysolutions.dscatalog.services.exceptions.DatabaseException;
 import com.exactorysolutions.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -23,6 +26,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true) 
 	public Page<ProductDTO> findAllPaged(PageRequest pageable) {
@@ -42,11 +48,7 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		entity.setName(dto.getName());
-		entity.setDescription(dto.getDescription());
-		entity.setPrice(dto.getPrice());
-		entity.setImg_url(dto.getImg_url());
-		entity.setDate(dto.getDate());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		
 		return new ProductDTO(entity, entity.getCategories());
@@ -56,12 +58,7 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product entity = repository.getOne(id);
-			entity.setName(dto.getName());
-			entity.setDescription(dto.getDescription());
-			entity.setPrice(dto.getPrice());
-			entity.setImg_url(dto.getImg_url());
-			entity = repository.save(entity);
-			entity.setDate(dto.getDate());
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
 			
 			return new ProductDTO(entity, entity.getCategories());
@@ -80,6 +77,21 @@ public class ProductService {
 		}
 		catch (DataIntegrityViolationException e) { 
 			throw new DatabaseException("Integrity Violation");
+		}
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImg_url(dto.getImg_url());
+		entity.setDate(dto.getDate());
+		
+		entity.getCategories().clear();
+		
+		for (CategoryDTO catDTO : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDTO.getId());
+			entity.getCategories().add(category);
 		}
 	}
 }
