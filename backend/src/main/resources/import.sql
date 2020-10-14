4b1376143872ceeed08a4fc096836245bcc05e8f
Marcelo Santos <margersan@gmail.com>
2020-10-14 18:39:16 -0300

    CRUD completo de clientes

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -54,3 +54,14 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (22, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (23, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (24, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
+
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Marcelo Santos', '278.278.278-27', TIMESTAMP WITH TIME ZONE '1972-10-30T06:34:00.12345Z', 5000.00, 3);
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Vera Aleandri', '135.135.135-13', TIMESTAMP WITH TIME ZONE '1971-03-18T10:45:00.12345Z', 4000.00, 3);
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Marcella Aleandri', '308.308.308-30', TIMESTAMP WITH TIME ZONE '2002-06-24T07:45:00.12345Z', 1030.30, 0);
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Matheus Aleandri', '307.307.307-30', TIMESTAMP WITH TIME ZONE '2006-07-20T11:55:00.12345Z', 1020.20, 0);
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Emanuell Aleandri', '268.268.268-26', TIMESTAMP WITH TIME ZONE '2011-12-10T04:15:00.12345Z', 1010.10, 0);
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Moises Santos', '035.035.035-03', TIMESTAMP WITH TIME ZONE '1935-08-01T23:18:00.12345Z', 2134.44, 5);
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Neide Ferreira', '040.040.040-04', TIMESTAMP WITH TIME ZONE '1942-05-30T17:24:00.12345Z', 1040.40, 5);
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Aparecida Caserta', '038.038.038-03', TIMESTAMP WITH TIME ZONE '1941-05-11T09:17:00.12345Z', 1050.50, 6);
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Marcio Santos', '118.118.118-18', TIMESTAMP WITH TIME ZONE '1965-10-16T12:30:00.12345Z', 5000.00, 1);
+INSERT INTO tb_client (name, cpf, birth_Date, income, children) VALUES ('Gilda Aleandri', '075.075.075-07', TIMESTAMP WITH TIME ZONE '1969-09-09T14:50:00.12345Z', 3560.00, 1);
